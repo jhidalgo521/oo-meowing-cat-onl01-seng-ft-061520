02e23bd0fd74fd560ff6b117c419664ae6cce20c
Jose Hidago <jhidalgo521@gmail.com>
2020-06-26 12:38:18 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,17 +1,11 @@
 ## code your solution here. 
 
-
-# class
 def Cat 
-  
-# getter and setter use macro attr_accessible
   attr_accessor :name 
 
-# puts meow!
-
- def meow
-   puts "meow!"
- end
+  def meow
+    puts "meow!"
+  end
   
 end
 
